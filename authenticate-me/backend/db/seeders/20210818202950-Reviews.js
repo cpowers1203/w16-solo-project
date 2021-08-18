5b11963d7c52df554bc71b9226cc0f346ed300f9
cpowers1203 <81358233+cpowers1203@users.noreply.github.com>
2021-08-18 15:43:07 -0500

    now finished with the seed files

---
+++ authenticate-me/backend/db/seeders/20210818202950-Reviews.js
@@ -0,0 +1,72 @@
+'use strict';
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Reviews', [
+      {
+        userId: 3,
+        spotId: 1,
+        review: 'Extremely spooky, needed my blankey!'
+      },
+       {
+        userId: 4,
+        spotId: 1,
+        review: 'Was absolutely shaking in my boots!'
+      },
+        {
+        userId: 5,
+        spotId: 2,
+        review: 'This place is as spooky as you think it is!!'
+      },
+       {
+        userId: 6,
+        spotId: 2,
+        review: 'Dont come here alone!'
+      },
+        {
+        userId: 2,
+        spotId: 3,
+        review: 'Heard someone calling my name the whole night dont follow the voice!'
+      },
+       {
+        userId: 6,
+        spotId: 3,
+        review: 'Still having nightmares about this place!'
+      },
+        {
+        userId: 2,
+        spotId: 4,
+        review: 'Saw a women in a white dress behind the house! Then poof shes gone!'
+      },
+      {
+        userId: 3,
+        spotId: 4,
+        review: 'The most haunted place ive ever been in!!'
+      },
+        {
+        userId: 4,
+        spotId: 5,
+        review: 'Heard whispers, and footsteps outside the door the whole time!'
+      },
+        {
+        userId: 6,
+        spotId: 5,
+        review: 'These ghosts where running a track meet in the house!!'
+      },
+       {
+        userId: 5,
+        spotId: 6,
+        review: 'Woke up in a cold sweat to someone staring at me in the corner of my room!'
+      },
+       {
+        userId: 3,
+        spotId: 6,
+        review: 'The ghost took my money!!'
+      },
+    ]);
+  },
+
+  down: (queryInterface, Sequelize) => {
+   return queryInterface.bulkDelete('Reviews', null, {});
+  }
+};
